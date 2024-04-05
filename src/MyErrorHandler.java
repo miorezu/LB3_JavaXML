8cab787b7bdba3d020bc321b630e1ead07111894
miorezu <m.miorezu@gmail.com>
2024-04-05 21:21:10 +0300

    Second Task

---
+++ src/MyErrorHandler.java
@@ -0,0 +1,24 @@
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+public class MyErrorHandler implements ErrorHandler {
+    @Override
+    public void warning(SAXParseException e) throws SAXException {
+        System.err.println("Warning:   " + e);
+        System.err.println("\tLine = " + e.getLineNumber() + "\tcolumn = " + e.getColumnNumber());
+    }
+
+    @Override
+    public void error(SAXParseException e) throws SAXException {
+        System.err.println("Error:   " + e);
+        System.err.println("\tLine = " + e.getLineNumber() + "\tcolumn = " + e.getColumnNumber());
+    }
+
+    @Override
+    public void fatalError(SAXParseException e) throws SAXException {
+        System.err.println("Fatal Error:   " + e);
+        System.err.println("\tLine = " + e.getLineNumber() + "\tcolumn = " + e.getColumnNumber());
+        System.exit(-1);
+    }
+}
---
+++ src/ValidatorXSD.java
@@ -0,0 +1,39 @@
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import java.io.File;
+import java.io.IOException;
+
+
+public class ValidatorXSD {
+    public static void validate(String xmlFileName, String xsdFileName) {
+        String language = XMLConstants.W3C_XML_SCHEMA_NS_URI;
+        SchemaFactory factory = SchemaFactory.newInstance(language);
+        Schema schema = null;
+        try {
+            schema = factory.newSchema(new File(xsdFileName));
+            javax.xml.validation.Validator validator = schema.newValidator();
+            ErrorHandler errorHandler = new MyErrorHandler();
+            validator.setErrorHandler(errorHandler);
+
+            Source source = new StreamSource(xmlFileName);
+            validator.validate(source);
+            System.out.println("The file " + xmlFileName + " is valid");
+        } catch (SAXException e) {
+            System.out.println("The file " + xmlFileName + " is NOT valid because");
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ValidatorXSD.validate("Popular_Baby_Names_NY.xml",
+                "Popular_Baby_Names_NY.xsd");
+    }
+}
